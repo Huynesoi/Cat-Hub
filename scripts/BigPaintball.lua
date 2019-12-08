992c8c76fecfb977f1d96120aa6579e33d075ce2
Kyler G <58646028+Pasted0002@users.noreply.github.com>
2019-12-08 04:36:13 -0800

    Added KillAll

--- scripts/BigPaintball.lua
+++ scripts/BigPaintball.lua
@@ -1,6 +1,7 @@
--- Farewell Infortality.
--- Version: 2.82
+
+
 -- Instances:
+
 local BigPaintball = Instance.new("ScreenGui")
 local Main = Instance.new("Frame")
 local Top = Instance.new("Frame")
@@ -8,7 +9,12 @@ local CatHub = Instance.new("TextLabel")
 local Exit = Instance.new("TextButton")
 local gun = Instance.new("TextButton")
 local OPgunlabel = Instance.new("TextLabel")
+local bigboykiller = Instance.new("TextButton")
+local KillAllLabel = Instance.new("TextLabel")
+local ifkillall = Instance.new("TextLabel")
+
 --Properties:
+
 BigPaintball.Name = "Big Paintball"
 BigPaintball.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 BigPaintball.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
@@ -78,22 +84,64 @@ OPgunlabel.TextScaled = true
 OPgunlabel.TextSize = 14
 OPgunlabel.TextWrapped = true
 OPgunlabel.TextXAlignment = Enum.TextXAlignment.Left
+
+bigboykiller.Name = "bigboykiller"
+bigboykiller.Parent = Main
+bigboykiller.BackgroundColor3 = Color3.new(0.141176, 0.141176, 0.141176)
+bigboykiller.BorderSizePixel = 0
+bigboykiller.Position = UDim2.new(0.195348829, 0, 0.320945859, 0)
+bigboykiller.Size = UDim2.new(0, 38, 0, 33)
+bigboykiller.Font = Enum.Font.SourceSansLight
+bigboykiller.Text = ""
+bigboykiller.TextColor3 = Color3.new(1, 1, 1)
+bigboykiller.TextScaled = true
+bigboykiller.TextSize = 14
+bigboykiller.TextWrapped = true
+
+KillAllLabel.Name = "Kill All Label"
+KillAllLabel.Parent = Main
+KillAllLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+KillAllLabel.BackgroundTransparency = 1
+KillAllLabel.Position = UDim2.new(0, 0, 0.320945859, 0)
+KillAllLabel.Size = UDim2.new(0, 84, 0, 33)
+KillAllLabel.Font = Enum.Font.SourceSansLight
+KillAllLabel.Text = "Kill All:"
+KillAllLabel.TextColor3 = Color3.new(1, 1, 1)
+KillAllLabel.TextScaled = true
+KillAllLabel.TextSize = 14
+KillAllLabel.TextWrapped = true
+KillAllLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+ifkillall.Name = "ifkillall"
+ifkillall.Parent = Main
+ifkillall.BackgroundColor3 = Color3.new(1, 1, 1)
+ifkillall.BackgroundTransparency = 1
+ifkillall.Position = UDim2.new(0.0348837227, 0, 0.442567468, 0)
+ifkillall.Size = UDim2.new(0, 194, 0, 38)
+ifkillall.Font = Enum.Font.SourceSansLight
+ifkillall.Text = ""
+ifkillall.TextColor3 = Color3.new(1, 1, 1)
+ifkillall.TextScaled = true
+ifkillall.TextSize = 14
+ifkillall.TextWrapped = true
+ifkillall.TextXAlignment = Enum.TextXAlignment.Left
+
 -- Scripts:
-function SCRIPT_GXRV78_FAKESCRIPT() -- CatHub.Color 
-	local script = Instance.new('LocalScript')
-	script.Parent = CatHub
+
+local function EQXHYHD_fake_script() -- CatHub.Color 
+	local script = Instance.new('LocalScript', CatHub)
+
 	script.Parent.MouseEnter:Connect(function()
 		game:GetService("TweenService"):Create(script.Parent, TweenInfo.new(.3), {TextColor3 = Color3.fromRGB(255,255,255)}):Play()
 	end)
 	script.Parent.MouseLeave:Connect(function()
 		game:GetService("TweenService"):Create(script.Parent, TweenInfo.new(.3), {TextColor3 = Color3.fromRGB(168, 168, 168)}):Play()
 	end)
-
 end
-coroutine.resume(coroutine.create(SCRIPT_GXRV78_FAKESCRIPT))
-function SCRIPT_JXEY87_FAKESCRIPT() -- Exit.Exit and Color 
-	local script = Instance.new('LocalScript')
-	script.Parent = Exit
+coroutine.wrap(EQXHYHD_fake_script)()
+local function FICDTHR_fake_script() -- Exit.Exit and Color 
+	local script = Instance.new('LocalScript', Exit)
+
 	script.Parent.MouseButton1Down:Connect(function()
 		script.Parent.Parent.Parent.Parent:Destroy()
 	end)
@@ -103,12 +151,12 @@ function SCRIPT_JXEY87_FAKESCRIPT() -- Exit.Exit and Color
 	script.Parent.MouseLeave:Connect(function()
 		game:GetService("TweenService"):Create(script.Parent, TweenInfo.new(.3), {TextColor3 = Color3.fromRGB(255, 255, 255)}):Play()
 	end)
-
+	
 end
-coroutine.resume(coroutine.create(SCRIPT_JXEY87_FAKESCRIPT))
-function SCRIPT_PWTJ69_FAKESCRIPT() -- Main.Main Script 
-	local script = Instance.new('LocalScript')
-	script.Parent = Main
+coroutine.wrap(FICDTHR_fake_script)()
+local function DSJRHUK_fake_script() -- Main.Main Script 
+	local script = Instance.new('LocalScript', Main)
+
 	function dragify(Main)
 	dragToggle = nil
 	dragSpeed = .20 
@@ -148,12 +196,14 @@ function SCRIPT_PWTJ69_FAKESCRIPT() -- Main.Main Script
 	end)
 	end
 	dragify((script.Parent))
-
 end
-coroutine.resume(coroutine.create(SCRIPT_PWTJ69_FAKESCRIPT))
-function SCRIPT_IRZZ69_FAKESCRIPT() -- gun.LocalScript 
-	local script = Instance.new('LocalScript')
-	script.Parent = gun
+coroutine.wrap(DSJRHUK_fake_script)()
+local function QPJBOE_fake_script() -- gun.LocalScript 
+	local script = Instance.new('LocalScript', gun)
+
+	local mt = getrawmetatable(game)
+	setreadonly(mt, false)
+	local namecall = mt.__namecall
 	
 	local on = false
 	script.Parent.MouseButton1Down:Connect(function()
@@ -164,9 +214,103 @@ function SCRIPT_IRZZ69_FAKESCRIPT() -- gun.LocalScript
 			on = false
 			script.Parent.Text = ""
 		end
-		spawn(function()
+	end)
+	spawn(function()
+	while on do
+	local function killanigga(bigbrain, player)
+	if bigbrain and player then
+	    local A_1 = 
+	    {
+	        [1] = 
+	    {
+	        [1] = player.Humanoid, 
+	        [2] = bigbrain, 
+	        [3] = 144, 
+	        [4] = player.Character.Head.Position,
+	        [5] = false, 
+	        [6] = false, 
+	        [7] = false
+	    }, 
+	        [2] = 
+	    {
+	        [1] = false, 
+	        [2] = false, 
+	        [3] = false, 
+	        [4] = false, 
+	        [5] = false, 
+	        [6] = 2, 
+	        [7] = 2
+	    }
+	    }
+	    local Event = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["do damage"]
+	    Event:FireServer(A_1)
+	     end
+	end
+	
+	
+	local function findplayers()
+	    local jew = {}
+	
+	    for _,player in pairs(game:GetService('Players'):GetPlayers()) do
+	        if player.Character and player ~= game:GetService('Players').LocalPlayer then
+	            local noob = false
+	            if player.Neutral then
+	                noob = true
+	            elseif Player.Team ~= game:GetService('Players').LocalPlayer.Team then
+	                noob = true
+	            end
+	            if noob and player.Character:FindFirstChild("Humanoid") and player.Character:FindFirstChild("Head") and not player.Character:FindFirstChildOfClass("ForceField") then
+	                jew[#jew+1] = player
+	            end
+	        end
+	    end
+	
+	    
+	    
+	    function namecallfunction(self,...)
+		if on then
+	        local args = {...}
+	        if getnamecallmethod() == "FireServer" then
+	            if args[1] then
+	                if args[1][1] then
+	                    if #args[1][1] == 3 then
+	                        local jew = getplayer()
+	                        if #jew > 0 then
+	                            killanigga(args[1][1][2],jew[math.random(1,#jew)])
+	                        end
+	                    end
+	                end
+	            end
+	        end
+		end
+	        return namecall(self,unpack(args))
+	    end
+	end
+	
+	wait(1)
+	end
+	hookfunction(mt.__namecall, namecallfunction)
+	end)
+end
+coroutine.wrap(QPJBOE_fake_script)()
+local function XRVVL_fake_script() -- bigboykiller.KillAllScript 
+	local script = Instance.new('LocalScript', bigboykiller)
+
+	
+	
+	script.Parent.MouseButton1Down:Connect(function()
+	script.Parent.Text = "✓"
+	script.Parent.Parent.ifkillall.Text = "Left Click to start killing!"
+	
+	local mt = getrawmetatable(game)
+	local runservice = game:GetService("RunService")
+	local uis = game:GetService("UserInputService")
+	setreadonly(mt,false)
+	
+	local on = false
+	local namecall = mt.__namecall
+	
 	pcall(function()
-		while on do
 	    for i, v in pairs(getgc(true)) do
 	        if type(v) == "table" and rawget(v, "firerate") then
 	            v.firerate = 0.00001
@@ -177,12 +321,97 @@ function SCRIPT_IRZZ69_FAKESCRIPT() -- gun.LocalScript
 	            v.additionalspeed = 10
 	        end
 	    end
-	wait(6)
-	end
-	end)
-	end)
 	end)
 	
-
+	local function killanigga(code,player)
+	    if player and code then
+	        local A_1 = 
+	        {
+	                [1] = 
+	            {
+	                [1] = player.Character.Humanoid, 
+	                [2] = code, 
+	                [3] = 170, 
+	                [4] = player.Character.Head.Position, 
+	                [5] = false, 
+	                [6] = false, 
+	                [7] = false
+	            }, 
+	                [2] = 
+	            {
+	                [1] = false, 
+	                [2] = false, 
+	                [3] = false, 
+	                [4] = false, 
+	                [5] = false, 
+	                [6] = 2, 
+	                [7] = 2
+	            }
+	        }
+	        local Event = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["do damage"]
+	        Event:FireServer(A_1)
+	    end
+	end
+	
+	local function getplayer()
+	    local f = {}
+	    for _,player in pairs(game:GetService("Players"):GetPlayers()) do
+	        if player.Character and player ~= game:GetService("Players").LocalPlayer then
+	            local c = false
+	            if player.Neutral then
+	                c = true
+	            elseif player.Team ~= game:GetService("Players").LocalPlayer.Team then
+	                c = true
+	            end
+	            if c and player.Character:FindFirstChild("Humanoid") and not player.Character:FindFirstChildOfClass("ForceField") and player.Character:FindFirstChild("Head") then
+	                f[#f+1] = player
+	            end
+	        end
+	    end
+	    return f
+	end
+	
+	local function namecallfunction(self,...)
+	    local args = {...}
+	    if getnamecallmethod() == "FireServer" then
+	        if args[1] then
+	            if args[1][1] then
+	                if #args[1][1] == 3 then
+	                    local f = getplayer()
+	                    if #f > 0 then
+	                        killanigga(args[1][1][2],f[math.random(1,#f)])
+	                    end
+	                end
+	            end
+	        end
+	    end
+	    return namecall(self,unpack(args))
+	end
+	
+	runservice.RenderStepped:Connect(function()
+	    if on then
+	        if validfgwindow() then
+	        else
+	            on = false
+	            
+	        end
+	    end
+	end)
+	
+	uis.InputBegan:Connect(function(input)
+	    if input.KeyCode == Enum.KeyCode.E then
+	        if on then on = false else on = true end
+	       
+	    end
+	end)
+	
+	mt.__namecall = namecallfunction
+	setreadonly(mt,true)
+	
+	
+	end)
+	
+	
+	
 end
-coroutine.resume(coroutine.create(SCRIPT_IRZZ69_FAKESCRIPT))
+coroutine.wrap(XRVVL_fake_script)()
